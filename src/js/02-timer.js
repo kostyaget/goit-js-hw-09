6be85773eb17aa33c59ccb321ae72ae1d4dfbe74
kostyaget <progogyd@gmail.com>
2022-04-02 21:32:21 +0300

    +

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,9 +1,10 @@
-import flatpickr from "flatpickr";
+import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
-
+require('flatpickr/dist/themes/material_red.css');
+import Notiflix from 'notiflix';
 
 const dateSelector = document.getElementById('datetime-picker');
-const btnStart = document.querySelector('button[dta-start]');
+const btnStart = document.querySelector('button[data-start]');
 const daysNum = document.querySelector('span[data-days]');
 const hoursNum = document.querySelector('span[data-hours]');
 const minutesNum = document.querySelector('span[data-minutes]');
@@ -15,61 +16,61 @@ let selectedTime = 0;
 btnStart.disabled = true;
 
 const options = {
-    enableTime: true,
-    time_24hr: true,
-    defaultDate: new Date(),
-    minuteIncrement: 1,
-    onClose(selectedDates) {
-        selectedTime = selectedDates[0].getTime();
-        checkDate();
-    },
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    selectedTime = selectedDates[0].getTime();
+    checkDate();
+  },
 };
 
-const fr = flatpickr(dateSelector, options);
-btnStart.addEventListener('ckick', onStartClick);
+const fp = flatpickr(dateSelector, options);
+btnStart.addEventListener('click', onStartClick);
 
 function checkDate() {
-    if (currentTime >= selectedTime) {
-        Notiflix.Notify.failure('Please choose a date in the future!');
-    } else {
-        btnStart.disabled = false;
-        Notiflix.Notify.succes('Correct date was set. click Start to degin countdown.');
-    }
+  if (currentTime >= selectedTime) {
+    Notiflix.Notify.failure('Please choose a date in the future!');
+  } else {
+    btnStart.disabled = false;
+    Notiflix.Notify.success('Correct date was set. Click START to begin countdown.');
+  }
 }
 
 function onStartClick() {
-    const timer = setInterval(() => {
-        btnStart.disabled = true;
-        dateSelector.disabled = true;
-        let remainingTime = convertMs(selectedTime - Date.now());
-        if (remainingTime.seconds < 0) {
-            clearInterval(timer);
-            Notiflix.Report.success('Out of time!', 'Timer reached end.', 'OK');
-            dateSelector.disabled = falce;
-        } else {
-            renderTimer(remainingTime);
-        }
-    }, 1000);
+  const timer = setInterval(() => {
+    btnStart.disabled = true;
+    dateSelector.disabled = true;
+    let remainingTime = convertMs(selectedTime - Date.now());
+    if (remainingTime.seconds < 0) {
+      clearInterval(timer);
+      Notiflix.Report.success('Out of time!', 'Timer reached end.', 'OK');
+      dateSelector.disabled = false;
+    } else {
+      renderTimer(remainingTime);
+    }
+  }, 1000);
 }
 function addLeadingZero(value) {
-    return String(value).padStart(2, '0');
+  return String(value).padStart(2, '0');
 }
 function renderTimer({ days, hours, minutes, seconds }) {
-    daysNum.textContent = addLeadingZero(`${days}`);
-    hoursNum.textContent = addLeadingZero(`${hours}`);
-    minutesNum.textContent = addLeadingZero(`${minutes}`);
-    secondsNum.textContent = addLeadingZero(`${seconds}`);
+  daysNum.textContent = addLeadingZero(`${days}`);
+  hoursNum.textContent = addLeadingZero(`${hours}`);
+  minutesNum.textContent = addLeadingZero(`${minutes}`);
+  secondsNum.textContent = addLeadingZero(`${seconds}`);
 }
- 
+
 function convertMs(ms) {
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
 
-    const days = Math.floor(ms / day);
-    const hours = Math.floor((ms % day) / hour);
-    const minutes = Math.floor(((ms % day) % hour) / minute);
-    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-    return { days, hours, minutes, seconds };
-}
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  return { days, hours, minutes, seconds };
+} 
